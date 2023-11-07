ec7de20ad20790af901b5ab1173d0e7f767839a9
Muhammad Rizwan <96943208+MRizwan1057@users.noreply.github.com>
2023-11-08 01:32:47 +0500

    updated

--- react_website/src/components/Common.jsx
+++ react_website/src/components/Common.jsx
@@ -8,9 +8,12 @@ const Common = (props) => {
           <div className="col-md-6 col-10 mx-auto order-2 order-sm-1">
             <div className="banner_text">
               <h1>
-                {props.name} <strong>ToobiTech</strong>
+                {props.name} <strong>Riz1_Tech</strong>
               </h1>
-              <h3>We are the team of talented Developers making websites</h3>
+              <h3>
+                We are the team of talented Developers driving innovation and
+                efficiency
+              </h3>
               <div>
                 <NavLink
                   to={props.visit}
@@ -31,7 +34,7 @@ const Common = (props) => {
 
       <div className="jumbotron jumbotron-fluid">
         <div className="container text-center">
-          <h1 className="display-4">MRK PVT.</h1>
+          <h1 className="display-4">Riz1_Tech</h1>
           <p className="lead">
             A well known Web Designing and Web Development Comapny
           </p>
--- react_website/src/components/Footer.jsx
+++ react_website/src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <>
       <footer className="container-fluid footer navbar-fixed-bottom text-center mb-0 ">
         <p>
-          2022 <b>ToobiTech</b> All right resereb by ||Terms & Conditions
+          2023 All right resered | <b>Riz1_Tech</b>
         </p>
       </footer>
     </>
